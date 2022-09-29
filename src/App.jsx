ffe70ef394c96498d538f83ec017180e1b6ac152
Moussa SAGNA <moussa.sagna@externe.sacem.fr>
2022-09-29 18:09:53 +0200

    creation des composants

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-import LogoDevIcon from '@mui/icons-material/LogoDev';
-import { AppBar, Box, IconButton, Toolbar, Typography, } from '@mui/material';
-import { Stack } from '@mui/system';
-import { Link } from 'react-router-dom';
+import Navbar from './components/General/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Footer from './components/General/Footer';
 
 function App() {
   return (
-    <AppBar position="static">
-      <Toolbar variant='regular' sx={{
-        backgroundColor: '#8e4e4E',
-        justifyContent : 'space-between',
-        alignItems : 'center'
-      }}>
-        <Stack direction={'row'} spacing={1} sx={{
-          alignItems: 'center'
-        }}>
-          <Box sx={{
-            height : 50,
-            objectFit : 'contain'
-          }}>
-            <img src='./images/logo-white.png' className='logo'/>
-          </Box>
-          <Typography variant='h5' component={'h1'}>Mon site</Typography>
-        </Stack>
-        <Stack direction={'row'} spacing={2}>
-          <Link>Acceuil</Link>
-          <Link>Acceuil</Link>
-          <Link>Acceuil</Link>
-        </Stack>
-
-      </Toolbar>
-    </AppBar>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
